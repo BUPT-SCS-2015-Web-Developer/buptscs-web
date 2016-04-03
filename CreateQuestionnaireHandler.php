52afbd4c8b3c99d942f750348e2c2ada44666463
LinkinYoung <1143052146@qq.com>
2016-04-04 00:25:36 +0800

    修复了成功发布问卷后仍然报错的问题

--- CreateQuestionnaireHandler.php
+++ CreateQuestionnaireHandler.php
@@ -25,25 +25,25 @@
                 VALUES ('$table_id', '$table_title', '$table_create_user', now(), '$table_close_date', '0', '$table_filled')";
   $insert_result = $db->query($insert_query);
   
-  //CREATE TABLE `Questionnaire`.`asdfg` ( `gggg` VARCHAR NOT NULL , `sdffg` SET('aaaa','ssss','dddd','ffff','gggg') NOT NULL , `qwerty` ENUM('fff','egg','hah') NOT NULL ) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
   $insert_query = "CREATE TABLE `Questionnaire`.`$table_id` ( `User_ID` VARCHAR(10) NOT NULL ,";
-  echo $insert_query;
   $question = $_POST['questions'];
   for ($i=1; $i < count($question); $i++) { 
       $colum = $question[$i]['colum'];
       $colum_type = $question[$i]['type'];
-      $colum_set = $question[$i]['set'];
+      
       $insert_query .= " `$colum` ";
       if (!strcmp($colum_type,"text")) {
           $insert_query .= "VARCHAR(255) NOT NULL ,";
       } elseif (!strcmp($colum_type,"checkbox")) {
+          $colum_set = $question[$i]['set'];
           $insert_query .= "SET($colum_set) NOT NULL ,";
       } elseif (!strcmp($colum_type,"radio")) {
+          $colum_set = $question[$i]['set'];
           $insert_query .= "ENUM($colum_set) NOT NULL ,";
       }
   }
   $insert_query = rtrim($insert_query,',');
   $insert_query .= ") ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;";
   $insert_result = $db->query($insert_query);
-  echo json_encode(Sucseeded);
+  echo json_encode("Sucseeded!");
 ?>
