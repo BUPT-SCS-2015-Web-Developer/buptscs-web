10e1c2945fa4b811361b2d3846435b9304ffd271
carol233 <jie322007@163.com>
2016-04-10 22:57:52 +0800

    。。。

--- CreateProblemHandler.php
+++ CreateProblemHandler.php
@@ -16,19 +16,23 @@
   $db = new ezSQL_mysql($qs_user, $qs_password, $qs_database, $db_host);//这里应该怎么写啊啊啊
   $db->query("set names 'utf8'");
   
+  
+  
+  
+$conn = mysql_connect();
+mysql_select_db( 'problem', $conn );
   $ID = uniqid();
   $TestTitle = $_POST['TestTitle'];
   $CreateUser = $username;
   $LimitTime = $_POST['LimitTime'];
-  $insert_query = "INSERT INTO `information` (`ID`, `TestTitle`, `CreateUser`, `LimitTime`) 
+  $sql = "INSERT INTO `problem`.`information` (`ID`, `TestTitle`, `CreateUser`, `LimitTime`) 
                 VALUES ('$ID', '$TestTitle', '$CreateUser', '$LimitTime')";
-  $insert_result = $db->query($insert_query);
-  
- 
+  $result = mysql_query($sql,$conn);
+	if ($result){
+		echo "<script>alert('添加信息成功');</script>";
+	}
 
 
-$conn = mysql_connect();
-mysql_select_db( 'problem', $conn );
 $sql = "CREATE TABLE competition$ID( ".
        "problem_ID int NOT NULL AUTO_INCREMENT, ".
        "competition varchar(1000), ".
@@ -38,45 +42,55 @@ $sql = "CREATE TABLE competition$ID( ".
 	   "option4 varchar(100),".          
        "answer varchar(5), ".
 	   "score int(5), ".
-	   "destination varchar(1000); ";
-mysql_query( $sql, $conn );
+	   "destination varchar(1000)); ";
+$result = mysql_query($sql,$conn);
+	if ($result){
+		echo "<script>alert('创建题目信息表成功');</script>";
+	}
+
 
 $sql = "CREATE TABLE answer$ID( ".
        "student_ID varchar(15), ".
        "question_ID int, ".
-       "answer varchar(15); ";
-mysql_query( $sql, $conn );
+       "answer varchar(15)); ";
+$result = mysql_query($sql,$conn);
+	if ($result){
+		echo "<script>alert('创建学生答案表成功');</script>";
+	}
+
 
 $sql = "CREATE TABLE allscores$ID( ".
        "score int, ".
        "student_ID varchar(15), ".
        "use_time int, ".
 	   "start_time  bigint(20),".
-       "submit_time date; ";
-mysql_query( $sql, $conn );
-mysql_close($conn);
+       "submit_time date); ";
+$result = mysql_query($sql,$conn);
+	if ($result){
+		echo "<script>alert('创建分数表成功');</script>";
+	}
+
 		
-		
-		
-		
+			
     $problem = $_POST['problems'];
   
     for ($i=1; $i < count($problem); $i++) { 
-       $competition = $problem[$i]['competition'];
-       $option1 = $problem[$i]['option1'];
-	   $option2 = $problem[$i]['option2'];
-	   $option3 = $problem[$i]['option3'];
-	   $option4 = $problem[$i]['option4'];
-	   $answer = $problem[$i]['answer'];
-	   $score = $problem[$i]['score'];
-	   $destination = $problem[$i]['destination'];
-	   $insert_query = "INSERT INTO `competition` (`competition`, `option1`, `option2`, `option3`, `option4`, `answer`, `score`, `destination`) 
+		$competition = $problem[$i]['competition'];
+		$option1 = $problem[$i]['option1'];
+		$option2 = $problem[$i]['option2'];
+		$option3 = $problem[$i]['option3'];
+		$option4 = $problem[$i]['option4'];
+		$answer = $problem[$i]['answer'];
+		$score = $problem[$i]['score'];
+		$destination = $problem[$i]['destination'];
+		$sql = "INSERT INTO `problem`.`competition` (`competition`, `option1`, `option2`, `option3`, `option4`, `answer`, `score`, `destination`) 
                 VALUES ('$competition', '$option1', '$option2', '$option3', '$option4', '$answer', '$score', '$destination')";
-       $insert_result = $db->query($insert_query); 
+		$result = mysql_query($sql,$conn);
+		if ($result){
+			echo "<script>alert('题目信息添加成功');</script>";
+		} 
 	}
- 
-    $insert_query = rtrim($insert_query,',');
-    $insert_query .= "ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;";
-    $insert_result = $db->query($insert_query);
+	
+    mysql_close($conn);
     echo json_encode("Sucseeded!");
 ?>
