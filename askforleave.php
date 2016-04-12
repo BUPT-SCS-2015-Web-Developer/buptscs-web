605d4d5e0ea518b860cceed4c235ce636188831f
boke1208 <907886076@qq.com>
2016-04-12 16:12:10 +0800

    一部分变动和格式的调整

--- askforleave.php
+++ askforleave.php
@@ -4,21 +4,13 @@
 <meta charset="utf-8">
 </head>
 <?php
-	session_start();
-
-	if(!isset($_SESSION['username'])){
-		exit('illegal access!');
-	}
-?>
-
-<?php
     $student_ID = $_SESSION['username'];
     $DataTime = addslashes($_POST['time']);
 	$reason = addslashes($_POST['reason']);
 	if($DataTime==""||$reason=="")
 	{
 		echo "<script> alert('请填写完整信息！');</script>";
-		echo "<script language='javascript'>document.location = 'inschool.php';</script>";
+		echo "<script>document.location = 'inschool.php';</script>";
 		die;
 	}
 	else{
@@ -54,19 +46,19 @@
 			$insert_result = $db->query($insert_query);
 
 			if($insert_result){
-				echo "<script language='javascript'>alert('请假成功！'+</br>+'请尽早销假!');</script>";
-				echo "<script language='javascript'>document.location = 'outschool.php';</script>";
+				echo "<script>alert('请假成功！'+</br>+'请尽早销假!');</script>";
+				echo "<script>document.location = 'outschool.php';</script>";
 			}
 			else{
-				echo "<script language='javascript'>alert('请假失败，请联系管理员！');</script>";
-				echo "<script language='javascript'>document.location = 'inschool.php';</script>";
+				echo "<script>alert('请假失败，请联系管理员！');</script>";
+				echo "<script>document.location = 'inschool.php';</script>";
 
 			}
 		}
 		else
 		{
-			echo "<script language='javascript'>alert('您有未销假信息！');</script>";
-			echo "<script language='javascript'>document.location = 'outschool.php';</script>";
+			echo "<script>alert('您有未销假信息！');</script>";
+			echo "<script>document.location = 'outschool.php';</script>";
 		}
 	}
 ?>
--- changeconfirm.php
+++ changeconfirm.php
@@ -26,7 +26,6 @@
 	}
 	else
 	{
-
 		mysql_query("UPDATE `scs`.`leave_school` SET `confirm` = 'Yes' WHERE student_ID = '$student_ID'");
 		mysql_close($con);
 	}
